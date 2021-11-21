52946d56b3d5d69bdf007e52a275d09553b5c3e7
Hibimaru0022 <sherbet02@icloud.com>
2021-11-21 23:01:55 +0900

    APIKEYーを安全化

--- libs/client.js
+++ libs/client.js
@@ -3,5 +3,5 @@ import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
     serviceDomain: 'hibimaru',
-    apiKey: 'bdcc899856f94360a2110db281115f41081a',
+    apiKey: process.env.API_KEY,
 });
