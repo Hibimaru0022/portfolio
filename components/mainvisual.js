6aa061d913c621ca55b8a7fb82c6c71a478f328a
Hibimaru0022 <sherbet02@icloud.com>
2021-12-07 14:35:06 +0900

    fix

--- components/mainvisual.js
+++ components/mainvisual.js
@@ -45,19 +45,17 @@ const Mainvisual = () => {
     });
     return (
         <div className={`${styles.mv} mv`} id="mv">
-            <div className={styles.inner}>
-                <div className={styles.mv__inner}>
-                    <div className={styles.mv__catch}>
-                        <h1 className={`${styles.mv__maincopy} js-maincopy`}>
-                            Hello!
-                            <br />
-                            Everyday Creative
-                        </h1>
-                        <p className={`${styles.mv__subcopy} js-subcopy`}>Front-end development, flat illustration and motion design regardless of individuals or companies.</p>
-                    </div>
-                    <div className={`${styles.mv__anim} js-ph`}>
-                        <Lottie options={defaultOptions} />
-                    </div>
+            <div className={styles.mv__inner}>
+                <div className={styles.mv__catch}>
+                    <h1 className={`${styles.mv__maincopy} js-maincopy`}>
+                        Hello!
+                        <br />
+                        Everyday Creative
+                    </h1>
+                    <p className={`${styles.mv__subcopy} js-subcopy`}>Front-end development, flat illustration and motion design regardless of individuals or companies.</p>
+                </div>
+                <div className={`${styles.mv__anim} js-ph`}>
+                    <Lottie options={defaultOptions} />
                 </div>
             </div>
         </div>
