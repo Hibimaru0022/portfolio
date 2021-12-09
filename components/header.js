8fc3791ebe36b69d3a09a54855b1eeea580512fb
Hibimaru0022 <sherbet02@icloud.com>
2021-12-09 20:14:16 +0900

    Fix mainvisual layout

--- components/header.js
+++ components/header.js
@@ -13,74 +13,72 @@ const Header = () => {
 
     return (
         <header className={styles.header} id="header">
-            <div className={styles.inner}>
-                <div className={styles.navigation}>
-                    <h1 className={open ? styles.close : styles.logo}>
-                        <Link href="/">
-                            <a>
-                                <Image src="/img/logo.svg" alt="Hibimaru" width={182} height={34} />
-                            </a>
+            <div className={styles.navigation}>
+                <h1 className={open ? styles.close : styles.logo}>
+                    <Link href="/">
+                        <a>
+                            <Image src="/img/logo.svg" alt="Hibimaru" width={182} height={34} />
+                        </a>
+                    </Link>
+                </h1>
+                <nav className={styles.nav}>
+                    <ul className={`${styles.navmenu} ${styles.visible_wide}`}>
+                        <li>
+                            <Link href="/">
+                                <a>Home</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/works_list">
+                                <a>Works</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/about">
+                                <a>About</a>
+                            </Link>
+                        </li>
+                    </ul>
+                    <button className={`${styles.contact__btn} ${styles.visible_wide}`}>
+                        <Link href="/contact">
+                            <a>Contact</a>
                         </Link>
-                    </h1>
-                    <nav className={styles.nav}>
-                        <ul className={`${styles.navmenu} ${styles.visible_wide}`}>
-                            <li>
-                                <Link href="/">
+                    </button>
+                </nav>
+                <nav className={`${styles.menu__nrw} ${styles.visible_nrw}`}>
+                    {/* <Image className={open ? undefined : styles.menu__logo} src="/img/logo.svg" alt="Hibimaru" width={120} height={30} /> */}
+                    <div className={styles.menu__icon} onClick={() => menu()}>
+                        <span className={open ? styles.open_bar : styles.menu__btn}></span>
+                        <span className={open ? styles.open_bar : styles.menu__btn}></span>
+                    </div>
+                    <div className={open ? styles.menu__box : styles.close}>
+                        <h1 className={styles.menu__logo}>
+                            <Image src="/img/ph_menu_logo.svg" alt="Hibimaru" width={120} height={36} />
+                        </h1>
+                        <ul className={styles.menu__list}>
+                            <li className={styles.menu__list__col}>
+                                <Link href="./">
                                     <a>Home</a>
                                 </Link>
                             </li>
-                            <li>
+                            <li className={styles.menu__list__col}>
                                 <Link href="/works_list">
                                     <a>Works</a>
                                 </Link>
                             </li>
-                            <li>
+                            <li className={styles.menu__list__col}>
                                 <Link href="/about">
                                     <a>About</a>
                                 </Link>
                             </li>
+                            <li className={styles.menu__list__col}>
+                                <Link href="/contact">
+                                    <a>Contact</a>
+                                </Link>
+                            </li>
                         </ul>
-                        <button className={`${styles.contact__btn} ${styles.visible_wide}`}>
-                            <Link href="/contact">
-                                <a>Contact</a>
-                            </Link>
-                        </button>
-                    </nav>
-                    <nav className={`${styles.menu__nrw} ${styles.visible_nrw}`}>
-                        {/* <Image className={open ? undefined : styles.menu__logo} src="/img/logo.svg" alt="Hibimaru" width={120} height={30} /> */}
-                        <div className={styles.menu__icon} onClick={() => menu()}>
-                            <span className={open ? styles.open_bar : styles.menu__btn}></span>
-                            <span className={open ? styles.open_bar : styles.menu__btn}></span>
-                        </div>
-                        <div className={open ? styles.menu__box : styles.close}>
-                            <h1 className={styles.menu__logo}>
-                                <Image src="/img/ph_menu_logo.svg" alt="Hibimaru" width={120} height={36} />
-                            </h1>
-                            <ul className={styles.menu__list}>
-                                <li className={styles.menu__list__col}>
-                                    <Link href="./">
-                                        <a>Home</a>
-                                    </Link>
-                                </li>
-                                <li className={styles.menu__list__col}>
-                                    <Link href="/works_list">
-                                        <a>Works</a>
-                                    </Link>
-                                </li>
-                                <li className={styles.menu__list__col}>
-                                    <Link href="/about">
-                                        <a>About</a>
-                                    </Link>
-                                </li>
-                                <li className={styles.menu__list__col}>
-                                    <Link href="/contact">
-                                        <a>Contact</a>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
+                    </div>
+                </nav>
             </div>
         </header>
     );
